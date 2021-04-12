6cfc5f4b574219728fc3371d91bfeab5c4bc1778
Chagaj Sergey <chagaj42@gmail.com>
2021-04-12 18:37:03 +0300

    Add effect on botton tap

---
+++ GorillaRoasters/GorillaRoasters.Android/Effects/ButtonFocusedEffect.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GorillaRoasters.Droid.Effects;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("TileMode")]
+[assembly: ExportEffect(typeof(ButtonFocusedEffect), nameof(ButtonFocusedEffect))]
+
+namespace GorillaRoasters.Droid.Effects
+{
+    public class ButtonFocusedEffect : PlatformEffect
+    {
+        private Android.Graphics.Drawables.RippleDrawable _gradient;
+        protected override void OnAttached()
+        {
+            _gradient = (Android.Graphics.Drawables.RippleDrawable)Control.Background;
+            Control.SetOutlineSpotShadowColor(Android.Graphics.Color.Transparent);
+        }
+
+        protected override void OnDetached()
+        {
+        }
+        protected override void OnElementPropertyChanged(PropertyChangedEventArgs args)
+        {
+            if (args?.PropertyName == "IsPressed")
+            {
+                if (((Xamarin.Forms.Button)Element).IsPressed)
+                {
+                    Control.SetBackgroundColor(Android.Graphics.Color.Aquamarine);
+                }
+                else
+                    Control.SetBackground(_gradient);
+                
+            }
+        }
+    }
+}
---
+++ GorillaRoasters/GorillaRoasters/Effects/ButtonFocusedEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GorillaRoasters.Effects
+{
+    public class ButtonFocusedEffect : RoutingEffect
+    {
+        public ButtonFocusedEffect() : base($"TileMode.{nameof(ButtonFocusedEffect)}")
+        {
+        }
+    }
+}
